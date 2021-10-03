ec7e2d25dea2e96da87b0684a19d8cf4319ecd49
Anesh kumar <91781521+Aneshlohana@users.noreply.github.com>
2021-10-03 02:13:04 -0700

    Add files via upload

---
+++ fib series.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+void fib(int n)
+{
+	int t1=0;
+	int t2=1;
+	int next_term;
+	for(int i=1;i<=n;i++)
+	{
+		cout<<t1<<" ";
+		next_term=t1+t2;
+		t1=t2;
+		t2=next_term;
+	}
+	return;
+}
+int main()
+{
+	int n;
+	cout<<"Enter number for fibonic series:"<<endl;
+	cin>>n;
+	
+	fib(n);
+}
